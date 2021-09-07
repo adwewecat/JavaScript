eda50cbd282137de68d359b5f48e73a623cfcb11
adwewecat <89115538+adwewecat@users.noreply.github.com>
2021-09-07 10:40:55 +0700

    'Nguyen-Anh-Dung'

---
+++ join-project-new.bat
@@ -0,0 +1,11 @@
+CD /d d:\LAP_TRINH_VIP\4.F8.edu.vn\JavaScript
+
+git init
+git add -A
+git commit -m 'Nguyen-Anh-Dung'
+git remote add origin https://github.com/adwewecat/JavaScript.git
+git push origin master
+
+Echo Chao Tam Biet Nhe
+
+PAUSE
